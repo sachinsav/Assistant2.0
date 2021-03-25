5df2ac082eb850410d8bcd3a49b3274b5d904acd
sachinsav <sachin.saw.13@gmai.com>
2021-03-25 17:59:54 +0530

    splash screen chage animation add

--- app/src/main/java/com/ideotic/edioticideas/aaya/Splash.java
+++ app/src/main/java/com/ideotic/edioticideas/aaya/Splash.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 /**
  * Created by Mukul on 13-05-2016.
@@ -13,12 +16,14 @@ import android.os.Handler;
 public class Splash extends Activity {
 
     private static int SPLASH_TIMER_COUNT = 5000;
-
+//    private LottieAnimationView lan;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+//        lan = findViewById(R.id.imageViewLogo);
+//        lan.setVisibility(View.VISIBLE);
+//        lan.playAnimation();
         new Handler().postDelayed(new Runnable() {
 
             //showing splash screen with timer..
